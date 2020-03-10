629b3d3f3af42cdbe426a5aff518fd4b0cc5ea95
Alvin Mwaura <alvinmwaura@Alvins-MacBook-Pro.local>
2020-03-10 17:05:06 -0400

    commit on 'answer'

---
+++ tests/operation.test.js
@@ -0,0 +1,19 @@
+const describe = require('mocha').describe
+const it = require('mocha').it
+const expect = require('chai').expect
+const strictEqual = require('../operation')
+
+describe('function', () => {
+    describe('strictEqual', () => {
+        it('returns the boolean of all the values provided', () => {
+            //create the test data 
+            const operands = [20, 50] == operand['20', '50']
+
+            //call the function with test data
+            const Boolean = Boolean(operands)
+
+            //make assertions.what should be done 
+            expect(boolean).to.deep.equal(true)
+        })
+    }
+})
