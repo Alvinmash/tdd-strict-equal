1e0b93b94a34b3d99369ab652645d26ca2c8c67b
Alvin Mwaura <alvinmwaura@Alvins-MacBook-Pro.local>
2020-03-16 21:46:20 -0400

    commit on 'answer'

--- operation.js
+++
@@ -1,6 +0,0 @@
-const strictEqual = (Boolean) => {
-    const operands = (20, 50) operand = ('20', '50')
-    object1.key == object2.key
-}
-
-module.exports = Boolean('20', '50') == (20, 50)
---
+++ tests/function.test.js
@@ -0,0 +1,32 @@
+const describe = require('mocha').describe
+const it = require('mocha').it
+const expect = require('chai').expect
+const strictEqual = require('../function')
+
+
+describe('function', () => {
+  describe('strictEqual', () => {
+    it('returns true when given two parameters of the same type of value', () => {
+      const boolean = strictEqual(50, 50)
+      expect(boolean).to.equal(true)
+    })
+
+    it('returns false when given two parameters of the different type of value', () => {
+      const boolean = strictEqual('50', 50)
+      expect(boolean).to.equal(false)
+    })
+
+    it('returns false when given two parameters of the different type of value', () => {
+      const boolean = strictEqual(50, '3')
+      expect(boolean).to.equal(false)
+    })
+  })
+})
+
+
+
+
+
+
+
+
