f36e9e7a4f9bc742995d939c90b8f6248160c812
Alvin Mwaura <alvinmwaura@Alvins-MacBook-Pro.local>
2020-03-17 15:48:21 -0400

    commit on answer

--- tests/function.test.js
+++ tests/function.test.js
@@ -7,16 +7,19 @@ const strictEqual = require('../function')
 describe('function', () => {
   describe('strictEqual', () => {
     it('returns true when given two parameters of the same type of value', () => {
+      // create test data 
       const boolean = strictEqual(50, 50)
       expect(boolean).to.equal(true)
     })
 
     it('returns false when given two parameters of the different type of value', () => {
+      //call the function with the test data 
       const boolean = strictEqual('50', 50)
       expect(boolean).to.equal(false)
     })
 
     it('returns false when given two parameters of the different type of value', () => {
+      //make assertions about what should be true 
       const boolean = strictEqual(50, '3')
       expect(boolean).to.equal(false)
     })
